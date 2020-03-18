259de228649ab0d8b918aabf36ea032ae8d4ef1a
Sabine <sabine@21.lv>
2020-03-18 23:27:47 +0100

    img displayed but not playing

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const i = 0; //Starting point
+let i = 0; //Starting point
 const images = [];
 const time = 3000; //time between slides
 
@@ -9,3 +9,16 @@ images[0] = 'image3.png';
 images[0] = 'image4.png';
 
 //change image
+
+function changeImg() {
+    document.slide.src = images[i]; //gets first
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("changeImg(), time");
+}
+
+window.onload = changeImg;
