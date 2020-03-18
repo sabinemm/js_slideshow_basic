6bc860dbbfc940328cd0bcbba72a3e1771d5b865
Sabine <sabine@21.lv>
2020-03-18 23:13:41 +0100

    added all files

---
+++ main.js
@@ -0,0 +1,11 @@
+const i = 0; //Starting point
+const images = [];
+const time = 3000; //time between slides
+
+// image list
+images[0] = 'image1.png';
+images[0] = 'image2.png';
+images[0] = 'image3.png';
+images[0] = 'image4.png';
+
+//change image
